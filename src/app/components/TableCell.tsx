30d1f1a496322725ccd3f01a8165e71f0b4570e7
amr abd alhammed <amrabdalhameed947@gmail.com>
2025-05-01 21:52:32 +0300

    adding new data

--- src/app/components/TableCell.tsx
+++ src/app/components/TableCell.tsx
@@ -4,14 +4,24 @@ type PropsType = {
   char1: string;  
   char2: string;  
   char3: string;  
+  char4?: string;  
+  char5?: string;
+  char6?: string;
 };  
 
-const TableCell = ({ char1, char2, char3 }: PropsType) => {  
+const TableCell = ({ char1, char2, char3, char4, char5, char6 }: PropsType) => {  
   return (  
     <div className='cell'>  
       <p>{char1}</p>  
       <p>{char2}</p>  
-      <p>{char3}</p>  
+      <p>{char3}</p> 
+      {char4 && char5 && char6 && (
+        <>
+          <p>{char4}</p>
+          <p>{char5}</p>
+          <p>{char6}</p>
+        </>
+      )}
     </div>  
   );  
 };  
--- src/app/components/TableRow.tsx
+++ src/app/components/TableRow.tsx
@@ -13,7 +13,7 @@ const TableRow = () => {
      
        <TableCell char1={ele.prop3.char1} char2={ele.prop3.char2} char3={ele.prop3.char3} />
        <TableCell char1={ele.prop4.char1} char2={ele.prop4.char2} char3={ele.prop4.char3} />
-       <TableCell char1={ele.prop5.char1} char2={ele.prop5.char2} char3={ele.prop5.char3} />
+       <TableCell char1={ele.prop5.char1} char2={ele.prop5.char2} char3={ele.prop5.char3} char4={ele.prop5.char4} char5={ele.prop5.char5} char6={ele.prop5.char6}/>
        <TableCell char1={ele.prop6.char1} char2={ele.prop6.char2} char3={ele.prop6.char3} />
     
      </>
