bc558cb2c433c1ff44278440993f59992cdc237d
amr abd alhammed <amrabdalhameed947@gmail.com>
2025-05-09 15:17:23 +0300

    finally release of style one

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -6,18 +6,18 @@ const Table = () => {
     <div className="w-full overflow-x-auto">
       <div className="min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg">
         <div className="son">
-          <div className="p-[8px]  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
+          <div className="p-[6px] rounded-[11px] text-2xl  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
             الحرف
           </div>
-          <div className="p-[8px]  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
+          <div className="p-[6px] rounded-[11px] text-2xl  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
             المخرج
           </div>
-          <div className="p-[8px]  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
+          <div className="p-[6px] rounded-[11px] text-2xl  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
             صفاته
           </div>
 
-          <div className="p-[8px]  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
-            متوى قوة الحرف
+          <div className="p-[6px] rounded-[11px] text-2xl  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
+            مستوى قوة الحرف
           </div>
         </div>
         <div className="parent border-collapse">
--- src/app/components/TableCell.tsx
+++ src/app/components/TableCell.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type PropsType = {
-  char1: string;
+  char1?: string;
   char2: string;
   char3: string;
   char4?: string;
@@ -11,7 +11,7 @@ type PropsType = {
 
 const TableCell = ({ char1, char2, char3, char4, char5, char6 }: PropsType) => {
   return (
-    <div className="space-y-1">
+    <div className="space-y-1 ">
       <p className="  text-gray-900 dark:text-gray-100">{char1}</p>
       <p className=" text-gray-600 dark:text-gray-300">{char2}</p>
       <p className=" text-gray-600 dark:text-gray-300">{char3}</p>
