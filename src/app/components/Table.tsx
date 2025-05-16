192c54ffff68ed5694cf8d38dcd8d5f75e04e8ae
amr abd alhammed <amrabdalhameed947@gmail.com>
2025-05-16 11:56:55 +0300

    optimizing mobile screen

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -3,7 +3,7 @@ import TableRow from "./TableRow";
 
 const Table = () => {
   return (
-    <div className="w-full overflow-x-auto">
+    <div className="w-full overflow-x-auto bigDad">
       <div className="min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg">
         <div className="son">
           <div className="p-[6px] rounded-[11px] text-2xl  border border-gray-200 dark:border-gray-600 font-semibold bg-gray-100 dark:bg-gray-700">
